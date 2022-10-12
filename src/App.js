c926ce16bee2a729ec523de340e0867abbbfcbd2
Jennynny <101732141+Jennynny@users.noreply.github.com>
2022-10-12 22:31:57 +0300

    weather app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,70 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useState } from 'react';
+
+const api = {
+  key: "1b5a3319844b88508ae229aa7685bbd2",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+
 
 function App() {
+  const ICON_URL = 'http://openweathermap.org/img/wn/';
+  const [search, setSearch] = useState('');
+  const [weather, setWeather] = useState({});
+  const searchPressed = () => {
+   fetch(`${api.base}weather?q=${search}&units=metric&APPID=${api.key}`)
+   .then((response) => response.json())
+   .then((result) => {
+    console.log(result);
+    setWeather(result);
+   });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {/* Header */}
+       <h1>Weather</h1>
+
+        {/* Search Box */} {/* Placeholder text */}
+        <div>
+          <input type="text" 
+          placeholder='Enter city...' 
+          onChange={(e) => setSearch(e.target.value)}
+          />
+          <button onClick={searchPressed}>Search</button>
+        </div>
+      
+      {typeof weather.main !== 'undefined' ? (
+        <div>
+
+          {/*Location: City and country*/}
+          <p>{weather.name} ({weather.sys.country})</p>
+
+          {/* Temperature °C */}
+          <p>{weather.main.temp} °C</p>
+
+          {/* Feels Like °C */}
+          <p>Feels Like {weather.main.feels_like} °C</p>
+
+          {/* Condition (Sunny,clouds etc. ) */}
+          <p>{weather.weather[0].main}</p>
+          <p>({weather.weather[0].description})</p>
+
+          {/* Humidity */}
+          <p>Humidity {weather.main.humidity} %</p>
+
+          {/*Image Icon*/}
+          <img src={ICON_URL + weather.weather[0].icon + '@2x.png'}alt=""/>
+
+        </div>
+
+        ) : (
+        ""
+      )}
+
       </header>
     </div>
   );
